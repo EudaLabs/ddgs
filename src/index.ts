53eb4721a72770d3eda34aae870e06dde19b25e4
byigitt <c2228009@student.cankaya.edu.tr>
2024-11-16 13:26:45 +0300

    feat(src): add initial DuckDuckGo service exports

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { DuckDuckGoService as default } from './services';
+export type { DuckDuckGoConfig } from './services/base';
+export type { ImageResult } from './services/images';
+export type { VideoResult } from './services/videos';
+export type { NewsResult } from './services/news';
+export { DuckDuckGoError, ErrorType, ErrorCodes } from './errors';
